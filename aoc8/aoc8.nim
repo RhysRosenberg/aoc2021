2cf634210126118074bc3b409e558658f2955022
Rhys Rosenberg <rhys.rosenberg@gmail.com>
2021-12-08 02:18:51 -0500

    day 8 oh god

--- aoc8/aoc8.nim
+++ aoc8/aoc8.nim
@@ -1,6 +1,11 @@
 import std/sequtils
 import std/strutils
 import std/tables
+import std/sets
+import std/math
+
+# WARNING: HERE BE DRAGONS
+
 const part = 2
 var 
     input = readFile("input.txt").splitLines()
@@ -15,21 +20,53 @@ if part == 1:
         cnt += suboutputs.filter(closure).len
     echo cnt
 else:
-    var answertbl = initTable[string, seq[int]]()
-    for signalpattern in signalpatterns:
+    var total: int = 0
+    var answertbl = initTable[HashSet[char], int]()
+    var oneswires, fourwires, lshape: HashSet[char]
+    for t in zip(signalpatterns, outputs):
+        var 
+            signalpattern = t[0]
+            output = t[1]
         for signal in signalpattern:
             case signal.len:
                 of 2:
-                    answertbl[signal] = @[1]
+                    answertbl[signal.toHashSet] = 1
+                    oneswires = signal.toHashSet
                 of 3:
-                    answertbl[signal] = @[7]
+                    answertbl[signal.toHashSet] = 7
                 of 4:
-                    answertbl[signal] = @[4]
-                of 5:
-                    answertbl[signal] = @[2,3,5]
-                of 6:
-                    answertbl[signal] = @[6,9]
+                    answertbl[signal.toHashSet] = 4
+                    fourwires = signal.toHashSet
                 of 7:
-                    answertbl[signal] = @[8]
+                    answertbl[signal.toHashSet] = 8
                 else:
-                    "impossible"
+                    discard
+        lshape = fourwires - oneswires
+            # Now program a constraint solver lol
+        proc lenchecker(desired_len: int): (proc(x: string): bool) = 
+            return proc(x: string): bool = 
+                return x.len == desired_len
+        var horizontals: HashSet[char]
+        for signal in signalpattern.filter(lenchecker(5)):
+            var x = signal.toHashSet
+            if oneswires < x:
+                answertbl[signal.toHashSet] = 3
+                horizontals = x - oneswires
+            elif lshape < x:
+                answertbl[signal.toHashSet] = 5
+            else:
+                answertbl[signal.toHashSet] = 2
+        for signal in signalpattern.filter(lenchecker(6)):
+            var x = signal.toHashSet
+            if oneswires < x and horizontals < x:
+                answertbl[signal.toHashSet] = 9
+            elif oneswires < x:
+                answertbl[signal.toHashSet] = 0
+            else:
+                answertbl[signal.toHashSet] = 6
+        
+        echo(answertbl)
+
+        for i in 0..3:
+            total += answertbl[output[i].toHashSet] * (10 ^ (3-i))
+    echo total
