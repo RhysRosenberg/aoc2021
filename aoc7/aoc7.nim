5b55eb9ba6f79008d2aa738231da382671dc48f1
Rhys Rosenberg <rhys.rosenberg@gmail.com>
2021-12-08 01:26:58 -0500

    add a part flip for day 7 lol

--- aoc7/aoc7.nim
+++ aoc7/aoc7.nim
@@ -1,5 +1,6 @@
 import std/sequtils
 import std/strutils
+const part = 1
 var 
     inp = readFile("input.txt").split(',').map(parseInt)
 
@@ -8,6 +9,10 @@ for x in 0..inp.max():
     proc triangle(i:int): int = 
         var dist = abs(i - x) 
         int(dist * (dist + 1) / 2)
-    var test = inp.map(triangle).foldl(a + b, 0)
+    var test = if part == 1: 
+            inp.map(proc(i:int):int = abs(i - x)).foldl(a + b, 0)
+        else:
+            inp.map(triangle).foldl(a + b, 0)
+
     m = min(test, m)
 echo m
